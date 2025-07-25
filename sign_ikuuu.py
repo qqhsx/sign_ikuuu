acf8e5e59453b2c61e74e96395097acdb1b783a2
qqhsx <56535272+qqhsx@users.noreply.github.com>
2025-07-25 13:51:31 +0800

    Update sign_ikuuu.py

--- sign_ikuuu.py
+++ sign_ikuuu.py
@@ -20,7 +20,7 @@ def handler(event=None, context=None):
     check_url = 'https://ikuuu.ch/user/checkin'
 
     header = {
-        'origin': 'https://ikuuu.one',
+        'origin': 'https://ikuuu.ch',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
     }
 
