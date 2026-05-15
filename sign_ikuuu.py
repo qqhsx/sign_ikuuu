0a6b8c7c80e77a2d88cf356382331bb1b81b8a1a
qqhsx <56535272+qqhsx@users.noreply.github.com>
2026-05-15 08:20:57 +0800

    Update login and check-in URLs in sign_ikuuu.py

--- sign_ikuuu.py
+++ sign_ikuuu.py
@@ -16,11 +16,11 @@ def handler(event=None, context=None):
 
     session = requests.session()
 
-    login_url = 'https://ikuuu.nl/auth/login'
-    check_url = 'https://ikuuu.nl/user/checkin'
+    login_url = 'https://ikuuu.win/auth/login'
+    check_url = 'https://ikuuu.win/user/checkin'
 
     header = {
-        'origin': 'https://ikuuu.nl',
+        'origin': 'https://ikuuu.win',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
     }
 
